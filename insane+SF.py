fbea72bea03b5a3e6190211e04921cb50be4a161
richard <richard@corkmate.com>
2020-04-18 03:58:10 +1000

    add 5 plasmenylcholines and plasmenylethalomines

--- insane+SF.py
+++ insane+SF.py
@@ -169,8 +169,23 @@ lipidsa.update({      # 1   2   3   4   5   6   7   8   9  10  11  12  13  14  1
     "GMO":  (moltype, " -   -   -   -   -   -  GL1 GL2 C1A C2A D3A C4A C5A  -   -   -   -   -   -   - "),
     
 ## Plasmenylcholines 
-    "DPPMC": (moltype, " -   -   -  NC3  -  PO4 GL1 GL2 C1A C2A C3A C4A  -   -  C1B C2B C3B C4B  -   - "),
-    
+    "DPPMC": (moltype, " -   -   -  NC3  -  PO4 GL1 N0 C1A C2A C3A C4A  -   -  C3 C2B C3B C4B  -   - "),
+    "POPMC": (moltype, " -   -   -  NC3  -  PO4 GL1 N0 C1A D2A C3A C4A  -   -  C3 C2B C3B C4B  -   - "),
+    "DOPMC": (moltype, " -   -   -  NC3  -  PO4 GL1 N0 C1A D2A C3A C4A  -   -  C3 D2B C3B C4B  -   - "),
+    "PIPMC": (moltype, " -   -   -  NC3  -  PO4 GL1 N0 C1A D2A D3A C4A  -   -  C3 C2B C3B C4B  -   - "),
+    "OIPMC": (moltype, " -   -   -  NC3  -  PO4 GL1 N0 C1A D2A D3A C4A  -   -  C3 D2B C3B C4B  -   - "),
+    "OEPMC": (moltype, " -   -   -  NC3  -  PO4 GL1 N0 C1A C2A D3A D4A C5A  -  C3 D2B C3B C4B  -   - "),
+    "PAPMC": (moltype, " -   -   -  NC3  -  PO4 GL1 N0 D1A D2A D3A D4A C5A  -  C3 C2B C3B C4B  -   - "),
+    "PUPMC": (moltype, " -   -   -  NC3  -  PO4 GL1 N0 D1A D2A D3A D4A D5A  -  C3 C2B C3B C4B  -   - "),
+    "POPME": (moltype, " -   -   -  NH3  -  PO4 GL1 N0 C1A D2A C3A C4A  -   -  C3 C2B C3B C4B  -   - "),
+    "DOPME": (moltype, " -   -   -  NH3  -  PO4 GL1 N0 C1A D2A C3A C4A  -  -   C3 D2B C3B C4B  -   - "),
+    "OIPME": (moltype, " -   -   -  NH3  -  PO4 GL1 N0 C1A D2A D3A C4A  -   -  C3 D2B C3B C4B  -   - "),
+    "OQPME": (moltype, " -   -   -  NH3  -  PO4 GL1 N0 C1A D2A D3A D4A C5A  -  C3 D2B C3B C4B  -   - "),
+    "OAPME": (moltype, " -   -   -  NH3  -  PO4 GL1 N0 D1A D2A D3A D4A C5A  -  C3 D2B C3B C4B  -   - "),
+    "OUPME": (moltype, " -   -   -  NH3  -  PO4 GL1 N0 D1A D2A D3A D4A D5A  -  C3 D2B C3B C4B  -   - "),
+
+    "IAPME": (moltype, " -   -   -  NH3  -  PO4 GL1 N0 D1A D2A D3A D4A C5A  -  C3 D2B D3B C4B  -   - "),
+    "IQPME": (moltype, " -   -   -  NH3  -  PO4 GL1 N0 C1A D2A D3A D4A C5A  -  C3 D2B D3B C4B  -   - "),
     
 })
 
