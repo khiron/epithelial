7ff3fcd8e251d0abbdd9bef74d11e8defd9eabd7
richard <richard@corkmate.com>
2020-04-22 13:14:48 +1000

    add phosphorylated lipids

---
+++ default.tcl
@@ -0,0 +1,8 @@
+mol modselect 0 0 not resname PW and not resname ION
+mol modcolor 0 0 Name
+display projection Orthographic
+mol modstyle 0 0 Beads 1 12
+axes location Off
+color Display Background white
+rotate x by 120
+rotate y by 45
